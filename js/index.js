87db001456f80c08e2eee4cd400002030f6284fe
tolentinoel <rckrleyn@gmail.com>
2020-11-22 19:10:51 -0800

    done

--- js/index.js
+++ js/index.js
@@ -21,6 +21,7 @@ function showList(bookArray){
 
 function renderList(bookObj){
     const ul = document.querySelector('ul')
+
     const li = document.createElement('li')
     ul.appendChild(li)
     li.innerText = `${bookObj.title}`
@@ -35,8 +36,8 @@ function showPanel(event, obj){
     }
     const pic = document.createElement('img')
     pic.setAttribute('src', `${obj.img_url}`)
-    const h4Title = document.createElement('h4')
-    h4Title.innerText = obj.title
+    const h3Title = document.createElement('h3')
+    h3Title.innerText = obj.title
     const h4Sub = document.createElement('h4')
     h4Sub.innerText = obj.subtitle
     const h4Author = document.createElement('h4')
@@ -45,14 +46,15 @@ function showPanel(event, obj){
     p.innerText = obj.description
 
     panel.appendChild(pic)
-    panel.appendChild(h4Title)
+    panel.appendChild(h3Title)
     panel.appendChild(h4Sub)
     panel.appendChild(h4Author)
     panel.appendChild(p)
 
     const userList = document.createElement('ul')
+    userList.id = 'list-of-likes'
     //below iterates over each username on the users array of each bookObj
-    obj.users.map(user => {
+    obj.users.forEach(user => {
         const userLi = document.createElement('li')
         userLi.innerText = user.username
         userList.appendChild(userLi)
@@ -63,49 +65,60 @@ function showPanel(event, obj){
     btn.innerText = "LIKE"
     panel.appendChild(btn)
     btn.addEventListener('click', () => {
-        if (btn.innerText == "LIKE"){
-            btn.innerText = "UNLIKE"
-            likeBook(obj, obj.id)
-        } else {
-            btn.innerText = "LIKE"
-            unlikeBook(obj, obj.id)
-        }
-        toggleLike(obj, obj.id)
+        toggleBtn(obj)
         })
 
     }
+//there maybe a better way but fetching twice dependent on toggle seems a long route.
+function toggleBtn(obj){
+    const theBtn = document.querySelector('button')
+    const id = obj.id
+    const user = {"id":1, "username":"pouros"}
 
-function likeBook(obj, id){
-    fetch(userUrl)
-    .then(resp => resp.json())
-    .then(jsonStuff => {
-        let user1 = jsonStuff[0]
-        obj.users.push(user1)
-    })
+    if (theBtn.innerText == "LIKE"){
+        theBtn.innerText = "UNLIKE"
+        fetch(`${bookUrl}/${id}`, {
+            method: 'PATCH',
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: JSON.stringify({users: obj.users.concat(user)})
+          })
+          .then(res=>res.json())
+          .then(json=>addLike(obj))
+    } else {
+        theBtn.innerText = "LIKE"
+        fetch(`${bookUrl}/${id}`, {
+            method: 'PATCH',
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: JSON.stringify({users: obj.users.pop(user)})
+          })
+          .then(res=>res.json())
+          .then(json => removeLike(obj))
+
+    }
 
 }
 
-function unlikeBook(obj, id){
-    fetch(userUrl)
-    .then(resp => resp.json())
-    .then(jsonStuff => {
-        let user1 = jsonStuff[0]
-        obj.users.delete(user1)
-    
-    })
+function addLike(book){
+    const user = {"id":1, "username":"pouros"}
+    const list = document.querySelector('#list-of-likes')
+    const newUser = document.createElement('li')
 
+    newUser.innerText = user.username
+    list.appendChild(newUser)
 }
-    //sends a PATCH request to http://localhost:3000/books/:id
-    //toggles between like/unlike the book by user1
-function toggleLike(obj, id){
-    fetch(`${bookUrl}/${id}`, {
-        method: 'PATCH',
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        },
-        body: JSON.stringify(obj.users)
-        })
-        .then(res=>res.json())
 
+function removeLike(book){
+    const user = {"id":1, "username":"pouros"}
+    const list = document.querySelector('#list-of-likes')
+//if user exists on list, remove it from list. this is knowing it's the last child on 
+//the list of users who liked the book.
+    if (user) {
+        list.removeChild(list.lastChild)
+    }
 }
